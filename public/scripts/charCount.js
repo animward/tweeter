db4c7c59e1365e030bedfde6cc7e255a9024383b
animward <animward@gmail.com>
2024-03-25 14:41:59 -0700

    cap number of characters at 140

--- public/scripts/charCount.js
+++ public/scripts/charCount.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
     $(".new-tweet textarea").on("input", function() {
         var tweetLength = $(this).val().length;
-        // counts the number of characters in the textarea and updates the counter
+        // caps the number of characters in the textarea to 140
+        if (tweetLength > 140) {
+            $(this).val($(this).val().substring(0, 140));
+            tweetLength = 140;
+        }
         var remainingChars = 140 - tweetLength;
         $(".counter").text(tweetLength + "/140");
     });  
